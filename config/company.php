f7cd139a7cb0d5f4bbc3171cdaf0495ae3857ba0
Ambex Developer <developer.7n5x@ambex.lv>
2026-09-15 13:36:41 +0300

    feat: set default company details to WEBYSOFT SYSTEMS, MB (Vilnius, Lithuania) in config/company.php and .env.example

--- config/company.php
+++ config/company.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'name' => env('COMPANY_NAME', env('FRONT_COMPANY_NAME', 'Crowdplay Entertainment N.V.')),
-    'number' => env('COMPANY_NUMBER', env('FRONT_COMPANY_REG_NUMBER', '164829')),
-    'address' => env('COMPANY_ADDRESS', env('FRONT_COMPANY_ADDR', 'Heelsumstraat 51, Willemstad, Curaçao')),
-    'email' => env('COMPANY_EMAIL', env('FRONT_COMPANY_EMAIL', 'support@crowdplay.net')),
-    'phone' => env('COMPANY_PHONE', env('FRONT_COMPANY_PHONE', '+357 22 123 456')),
-    'license' => env('COMPANY_LICENSE', env('FRONT_COMPANY_LICENSE', 'OGL/2026/184/0129')),
+    'name' => env('COMPANY_NAME', env('FRONT_COMPANY_NAME', 'WEBYSOFT SYSTEMS, MB')),
+    'number' => env('COMPANY_NUMBER', env('FRONT_COMPANY_REG_NUMBER', '308033902')),
+    'address' => env('COMPANY_ADDRESS', env('FRONT_COMPANY_ADDR', 'V. Nagevičiaus g. 3, LT-08237 Vilnius')),
+    'email' => env('COMPANY_EMAIL', env('FRONT_COMPANY_EMAIL', 'info@crowdplay.net')),
+    'phone' => env('COMPANY_PHONE', env('FRONT_COMPANY_PHONE', '+370 60 000 000')),
+    'license' => env('COMPANY_LICENSE', env('FRONT_COMPANY_LICENSE', null)),
 ];
